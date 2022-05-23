cbf4b53a05d578a2f2d23b5e24bb38b633e8a79a
Anujithu21 <104365128+Anujithu21@users.noreply.github.com>
2022-05-23 21:43:34 +0530

    new feature add

---
+++ ChatAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Anu Sankaranarayanan on 09/05/22.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
